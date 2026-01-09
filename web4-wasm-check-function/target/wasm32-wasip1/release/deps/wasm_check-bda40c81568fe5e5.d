5ce4b1a9e029c72489ab7aad9caae2321f789766
ennseg <114716799+ennseg@users.noreply.github.com>
2026-01-09 15:20:30 +0700

    Add files via upload

---
+++ web4-wasm-check-function/target/wasm32-wasip1/release/deps/wasm_check-bda40c81568fe5e5.d
@@ -0,0 +1,5 @@
+C:\Users\22866\wasm-check\target\wasm32-wasip1\release\deps\wasm_check-bda40c81568fe5e5.d: src\main.rs
+
+C:\Users\22866\wasm-check\target\wasm32-wasip1\release\deps\wasm_check-bda40c81568fe5e5.wasm: src\main.rs
+
+src\main.rs:
---
+++ web4-wasm-check-function/target/wasm32-wasip1/release/wasm-check.d
@@ -0,0 +1,1 @@
+C:\Users\22866\wasm-check\target\wasm32-wasip1\release\wasm-check.wasm: C:\Users\22866\wasm-check\src\main.rs
